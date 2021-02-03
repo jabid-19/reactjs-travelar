d681a219cb476f450b1cb3a792472d9a1bdde1d4
jabid-19 <jabid19191@gmail.com>
2021-02-03 08:28:32 +0600

    first commit

---
+++ build/precache-manifest.62d91fbd1476c2d03f66ac10b6b3a249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c8f5449843cefe2f2c6335a4ea24bf",
+    "url": "/ReactJs-Travelar-Website/index.html"
+  },
+  {
+    "revision": "cc039514cfcad66c5dee",
+    "url": "/ReactJs-Travelar-Website/static/css/main.243ca405.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/ReactJs-Travelar-Website/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ReactJs-Travelar-Website/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc039514cfcad66c5dee",
+    "url": "/ReactJs-Travelar-Website/static/js/main.37f1020d.chunk.js"
+  },
+  {
+    "revision": "91365fb3194cbf6f1538",
+    "url": "/ReactJs-Travelar-Website/static/js/runtime-main.75388dee.js"
+  }
+]);
